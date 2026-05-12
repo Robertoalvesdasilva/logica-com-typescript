40e3a3a7dc4cbc12cdd06c5e0d111ebc0234f6c5
Robertoalvesdasilva <156485424+Robertoalvesdasilva@users.noreply.github.com>
2026-05-12 07:46:19 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ const tela = require('readline-sync')
 const nome:string = tela.question('Qual e o seu nome?')
 const ano_nasc:number = tela.question('Qual o ano seu nascimento? ')
 const idade = 2026 - ano_nasc
-console.log('Olá, '+ nome +' a sua idade é '+ idade + 'anos.')
+console.log('Olá,' +nome+ 'o ano que voce nasceu foi' +ano_nasc+ 'a sua idade é' +idade+ 'anos.')
 
