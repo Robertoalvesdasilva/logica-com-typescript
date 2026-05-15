bd4c07b7f7e6376feefea19b20f40d7bb63faa94
roberto <pablorobertoa@gmail.com>
2026-05-15 11:18:25 -0300

    pasta de aprendizado do araay

--- arrays.ts
+++ arrays.ts
@@ -6,7 +6,28 @@ console.log("frutas",frutas.length)
 console.log("frutas",frutas[frutas.length - 1])
  
 //orden alfabetica
-frutas.sort()
+//frutas.sort()
+
+//orden decrecente
+//frutas.reverse()
+
+// acresenta frutas no final da fila
+//frutas.push("limão")
+
+// acresentar na primeira posição
+//frutas.unshift("Melão")
+
+
+// tracar a fruta da segunda posição pela melancia
+//frutas[2]="melancia"
+
+
+// remove o ultimo elemento do array 
+//frutas.pop()
+
+// remocve o primeiro elemento do array
+//frutas.shift()
+
 //carrega a lista
 
 for(let i = 0; i < frutas.length; i++){
